1adcb6d8c0388620e37e8d75b825ff65a3f8b897
Horimatsu Takuya <new_woory@hotmail.com>
2021-12-02 20:32:11 +0900

    fix: class change

---
+++ src/main/kotlin/com/github/horitaku1124/ts_compiler/nodes/FirstClassObjectNode.kt
@@ -0,0 +1,4 @@
+package com.github.horitaku1124.ts_compiler.nodes
+
+open class FirstClassObjectNode: NodeBase() {
+}
--- src/main/kotlin/com/github/horitaku1124/ts_compiler/nodes/StringNode.kt
+++ src/main/kotlin/com/github/horitaku1124/ts_compiler/nodes/StringNode.kt
@@ -1,6 +1,6 @@
 package com.github.horitaku1124.ts_compiler.nodes
 
-data class StringNode(var value: String): NodeBase() {
+data class StringNode(var value: String): FirstClassObjectNode() {
   override fun toString(): String {
     return "String: \"$value\""
   }
